485eab9d29e55858252603df7368fae6686de190
Nibras Ahmed <odaabultum116@gmail.com>
2025-12-01 20:26:02 +0300

    Add Tailwind CSS configuration file

---
+++ Tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./app/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: { sans: ["Inter", "sans-serif"] }
+    }
+  },
+  plugins: [],
+  darkMode: 'class'
+};
